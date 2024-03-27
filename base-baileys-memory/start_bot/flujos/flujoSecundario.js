0f80ca362f1c6dcb075ee7fd28466dccd7a9040c
Chistian <cris_7715@hotmail.com>
2024-03-27 16:03:48 -0500

    twlio version

--- base-baileys-memory/start_bot/flujos/flujoSecundario.js
+++
@@ -1,6 +0,0 @@
-const { addKeyword, addAnswer } = require('@bot-whatsapp/bot');
-const { createBot, createFlow, createProvider } = require('@bot-whatsapp/bot');
-
-
-//flujo Secundario
- const flujoSecundario = addKeyword(['Gracias', 'Muchas gracias']).addAnswer('De nada! 👌 Espero haberte ayudado')
---
+++ base-twilio-postgres/app.js
@@ -0,0 +1,29 @@
+const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
+
+const QRPortalWeb = require('@bot-whatsapp/portal')
+const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+const PostgreSQLAdapter = require('@bot-whatsapp/database/postgres')
+
+
+
+
+const main = async () => {
+    const adapterDB = new PostgreSQLAdapter({
+        host: POSTGRES_DB_HOST,
+        user: POSTGRES_DB_USER,
+        database: POSTGRES_DB_NAME,
+        password: POSTGRES_DB_PASSWORD,
+        port: POSTGRES_DB_PORT,
+    })
+    
+    const adapterFlow = createFlow([flowPrincipal])
+    const adapterProvider = createProvider(BaileysProvider)
+    createBot({
+        flow: adapterFlow,
+        provider: adapterProvider,
+        database: adapterDB,
+    })
+    QRPortalWeb()
+}
+
+main()
