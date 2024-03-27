d829d908f12bbf67df9aa55ee11e4dc03a1eee39
Chistian <cris_7715@hotmail.com>
2024-03-27 16:07:20 -0500

    cambios twilio

--- base-twilio-postgres/app.js
+++ base-twilio-postgres/app.js
@@ -1,29 +1,24 @@
 const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
 
-const QRPortalWeb = require('@bot-whatsapp/portal')
-const BaileysProvider = require('@bot-whatsapp/provider/baileys')
-const PostgreSQLAdapter = require('@bot-whatsapp/database/postgres')
-
-
+const TwilioProvider = require('@bot-whatsapp/provider/twilio')
+const MockAdapter = require('@bot-whatsapp/database/mock')
 
 
 const main = async () => {
-    const adapterDB = new PostgreSQLAdapter({
-        host: POSTGRES_DB_HOST,
-        user: POSTGRES_DB_USER,
-        database: POSTGRES_DB_NAME,
-        password: POSTGRES_DB_PASSWORD,
-        port: POSTGRES_DB_PORT,
-    })
-    
+    const adapterDB = new MockAdapter()
     const adapterFlow = createFlow([flowPrincipal])
-    const adapterProvider = createProvider(BaileysProvider)
+
+    const adapterProvider = createProvider(TwilioProvider, {
+        accountSid: 'YOUR_ACCOUNT_SID',
+        authToken: 'YOUR_ACCOUNT_TOKEN',
+        vendorNumber: '+14155238886',
+    })
+
     createBot({
         flow: adapterFlow,
         provider: adapterProvider,
         database: adapterDB,
     })
-    QRPortalWeb()
 }
 
 main()
