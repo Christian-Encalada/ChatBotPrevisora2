2a9c345f23e24a0b126ae38fdb69012f503ca312
Chistian <cris_7715@hotmail.com>
2024-03-27 16:19:42 -0500

    proceso...

--- base-twilio-postgres/app.js
+++ base-twilio-postgres/app.js
@@ -1,12 +1,17 @@
+require('dotenv').config(); // Cargar las variables de entorno desde el archivo .env
 const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
+const { POSTGRES_DB_HOST, POSTGRES_DB_USER, POSTGRES_DB_PASSWORD, POSTGRES_DB_NAME, POSTGRES_DB_PORT } = process.env;
 
 const TwilioProvider = require('@bot-whatsapp/provider/twilio')
 const MockAdapter = require('@bot-whatsapp/database/mock')
 
+const validarCedula = require('./config_bot/funciones/validarCedula')
+const flujoPrincipal = require('./config_bot/flujos/flujoPrincipal')
+const flujoSecundario = require('./config_bot/flujos/flujoSecundario')
 
 const main = async () => {
     const adapterDB = new MockAdapter()
-    const adapterFlow = createFlow([flowPrincipal])
+    const adapterFlow = createFlow([flujoPrincipal, flujoSecundario])
 
     const adapterProvider = createProvider(TwilioProvider, {
         accountSid: 'YOUR_ACCOUNT_SID',
---
+++ base-twilio-postgres/config_bot/flujos/flujoSecundario.js
@@ -0,0 +1,8 @@
+const { addKeyword, addAnswer } = require('@bot-whatsapp/bot');
+const { createBot, createFlow, createProvider } = require('@bot-whatsapp/bot');
+
+
+//flujo Secundario
+ const flujoSecundario = addKeyword(['Gracias', 'Muchas gracias']).addAnswer('De nada! 👌 Espero haberte ayudado')
+
+ module.exports = flujoSecundario;
